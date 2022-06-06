12c8190f6f639fece6b83dfa4148114f129f27d4
lu1um <emperor1812@gmail.com>
2022-06-06 13:28:24 +0900

    add_article_serializers

--- back/articles/views.py
+++ back/articles/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import Article, Comment
+
 
 
 def article_list_or_create(request):
@@ -18,4 +24,12 @@ def create_comment(request, article_pk):
 
 
 def comment_update_or_delete(request, article_pk, comment_pk):
+    pass
+
+
+def search(request):
+    pass
+
+
+def news_list_or_create(request):
     pass

8711085f7c2b8bbb7f4eab59270689b50d160f05
lu1um <emperor1812@gmail.com>
2022-06-06 09:58:47 +0900

    modify_article

--- travel_sketch/urls.py
+++ travel_sketch/urls.py
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/articles/', include('articles.urls')),
-    path('api/v1/accounts/', include('accounts.urls')),
-    path('api/v1/accounts/', include('dj_rest_auth.urls')),
-    path('api/v1/accounts/signup/', include('dj_rest_auth.registration, url')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
